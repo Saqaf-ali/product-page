09d4d4f51e2456443c9110acadf01efc82085f31
Saqaf <saqafaliabdo716@gmail.com>
2025-07-31 21:49:04 +0300

    feat:add event model

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,42 @@
 let modelExit=document.getElementById('modelExit');
 let modelCard=document.getElementById('modelCard');
 modelExit.addEventListener('click',function(){
-    modelCard.classList.toggle('')
+    modelCard.classList.toggle('hidden')
+    
 })
+
+function name(){
+    console.log('hi saqaf');
+    
+}
+
+// model
+
+let btnMor1=document.getElementById('btnMore1');
+console.log(btnMor1);
+
+btnMor1.addEventListener('click', function(){
+    // onsole.log(btnMor1);
+ modelCard.classList.remove('hidden');
+//  modelCard.classList.add('visible');
+ console.log(modelCard);
+ 
+    onsole.log(modelCard);
+});
+
+
+
+let btnMor2=document.getElementById('btnMore2');
+console.log(btnMor1);
+
+btnMor2.addEventListener('click', function(){
+    // onsole.log(btnMor1);
+ modelCard.classList.remove('hidden');
+//  modelCard.classList.add('visible');
+ console.log(modelCard);
+ 
+    onsole.log(modelCard);
+});
+
+
 
