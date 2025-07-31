23db9f627972c333b760ba636fa064a6608e7686
Saqaf <saqafaliabdo716@gmail.com>
2025-07-31 19:36:04 +0300

    feat : add button heidi model

---
+++ dist/main.js
@@ -0,0 +1,6 @@
+let modelExit=document.getElementById('modelExit');
+let modelCard=document.getElementById('modelCard');
+modelExit.addEventListener('click',function(){
+    modelCard.classList.toggle('')
+})
+
